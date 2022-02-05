dcf13be135350c0c4bea39ca2d94a4ef2c2c9b95
Gautier <gautier.blandin@gmail.com>
2022-02-05 19:00:35 +0100

    Add documentation for deleteNode function

--- src/tree_algorithms/utils.py
+++ src/tree_algorithms/utils.py
@@ -1,3 +1,5 @@
+# Delete node at node_index. This function assumes an UNDIRECTED graph. In the case of a directed graph, the node might
+# still in some edge lists
 def deleteNode(graph: dict[int, list[int]], node_index: int):
     neighbors = graph.pop(node_index)
     for neighbor_index in neighbors:
