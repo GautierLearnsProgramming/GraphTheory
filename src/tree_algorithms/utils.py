65046cd776c3cd56cca45aa86e3baaa100f94a9c
Gautier <gautier.blandin@gmail.com>
2022-02-06 18:38:24 +0100

    Add copyUndirectedGraph function to utils

--- src/tree_algorithms/utils.py
+++ src/tree_algorithms/utils.py
@@ -4,3 +4,7 @@ def deleteNode(graph: dict[int, list[int]], node_index: int):
     neighbors = graph.pop(node_index)
     for neighbor_index in neighbors:
         graph[neighbor_index].remove(node_index)
+
+
+def copyUndirectedGraph(graph: dict[int, list[int]]) -> dict[int, list[int]]:
+    return {node_index: [neighbor for neighbor in neighbors] for node_index, neighbors in graph.items()}
