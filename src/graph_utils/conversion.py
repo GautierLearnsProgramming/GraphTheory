4d88dd942427b4317d20592c83a199ff19617a4b
Gautier <gautier.blandin@gmail.com>
2022-07-08 11:28:20 +0200

    Add weighted ajd map to weighted adj list conversion function

--- src/graph_utils/conversion.py
+++ src/graph_utils/conversion.py
@@ -1,3 +1,5 @@
+WeightedEdge = tuple[int, float]
+
 def adj_map_to_adj_list(adj_map: dict[int, list[int]]) -> tuple[list[list[int]], dict[int, int]]:
     adj_list = []
     list_to_map = {}
@@ -18,8 +20,8 @@ def adj_list_to_adj_map(adj_list: list[list[int]], list_to_map: dict[int, int]):
 
 
 def weighted_adj_map_to_adj_list(
-        adj_map: dict[int, list[tuple[int, float]]]) -> tuple[list[list[tuple[int, float]]], dict[int, int]]:
-    adj_list: list[list[tuple[int, float]]] = []
+        adj_map: dict[int, list[WeightedEdge]]) -> tuple[list[list[WeightedEdge]], dict[int, int]]:
+    adj_list: list[list[WeightedEdge]] = []
     list_to_map = {}
     map_to_list = {}
     for index, node_index in enumerate(adj_map):
@@ -31,14 +33,14 @@ def weighted_adj_map_to_adj_list(
 
 
 def weighted_adj_list_to_adj_map(
-        adj_list: list[list[tuple[int, float]]], list_to_map: dict[int, int]) -> dict[int, list[tuple[int, float]]]:
-    adj_map: dict[int, list[tuple[int, float]]] = {}
+        adj_list: list[list[WeightedEdge]], list_to_map: dict[int, int]) -> dict[int, list[WeightedEdge]]:
+    adj_map: dict[int, list[WeightedEdge]] = {}
     for index, edges in enumerate(adj_list):
         adj_map[list_to_map[index]] = [(list_to_map[edge[0]], edge[1]) for edge in edges]
     return adj_map
 
 
-def weighted_adj_list_to_adj_matrix(adj_list: list[list[tuple[int, float]]]) -> list[list[float]]:
+def weighted_adj_list_to_adj_matrix(adj_list: list[list[WeightedEdge]]) -> list[list[float]]:
     adj_matrix = [[float('inf') for _ in range(len(adj_list))] for _ in range(len(adj_list))]
     for index, edges in enumerate(adj_list):
         for edge in edges:
@@ -46,11 +48,18 @@ def weighted_adj_list_to_adj_matrix(adj_list: list[list[tuple[int, float]]]) -> 
     return adj_matrix
 
 
-def weighted_adj_matrix_to_adj_list(adj_matrix: list[list[float]]) -> list[list[tuple[int, float]]]:
-    adj_list: list[list[tuple[int, float]]] = []
+def weighted_adj_matrix_to_adj_list(adj_matrix: list[list[float]]) -> list[list[WeightedEdge]]:
+    adj_list: list[list[WeightedEdge]] = []
     for node_index, edges_weights in enumerate(adj_matrix):
         adj_list.append([])
         for neighbor_index, edge_weight in enumerate(edges_weights):
             if edge_weight != float('inf'):
                 adj_list[node_index].append((neighbor_index, edge_weight))
     return adj_list
+
+
+def weighted_adj_map_to_unweighted_adj_map(adj_map: dict[int, list[WeightedEdge]]) -> dict[int, list[int]]:
+    unweighted_adj_map = {}
+    for node in adj_map:
+        unweighted_adj_map[node] = [child for (child, _) in adj_map[node]]
+    return unweighted_adj_map
