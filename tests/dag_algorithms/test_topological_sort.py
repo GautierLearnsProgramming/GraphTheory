fc913d03a8ee23621d18168e235a7702bd5964a4
Gautier <gautier.blandin@gmail.com>
2022-07-08 11:29:26 +0200

    Add topsort test

--- tests/dag_algorithms/test_topological_sort.py
+++ tests/dag_algorithms/test_topological_sort.py
@@ -1,12 +1,37 @@
-from src.dag_algorithms.topological_sort import topSort
+from src.dag_algorithms.topological_sort import topsort
+import src.graph_utils.conversion as conversion
+from typing import Callable
 
 
-def test_topSort():
-    dag = [
-        [1],
-        [2, 3],
-        [3],
-        []
-           ]
+def top_sort_tester(top_sort: Callable[[list[list[int]]], list[int]]):
+    graph = {
+        0: [(1, 5), (2, 1)],
+        1: [(5, 3), (4, 3), (3, 6)],
+        2: [(3, 2)],
+        3: [(4, 4)],
+        4: [(5, 1), (6, 6)],
+        5: [(6, 2)],
+        6: [(7, 2)],
+        7: []
+    }
 
-    assert topSort(dag) == [0, 1, 2, 3]
+    unweighted_graph = conversion.weighted_adj_map_to_unweighted_adj_map(graph)
+    adj_list, _ = conversion.adj_map_to_adj_list(unweighted_graph)
+    sorted_graph = top_sort(adj_list)
+
+    print(sorted_graph)
+
+    assert sorted_graph.index(0) < sorted_graph.index(1)
+    assert sorted_graph.index(0) < sorted_graph.index(2)
+    assert sorted_graph.index(2) < sorted_graph.index(3)
+    assert sorted_graph.index(3) < sorted_graph.index(4)
+    assert sorted_graph.index(1) < sorted_graph.index(3)
+    assert sorted_graph.index(1) < sorted_graph.index(4)
+    assert sorted_graph.index(1) < sorted_graph.index(5)
+    assert sorted_graph.index(4) < sorted_graph.index(5)
+    assert sorted_graph.index(4) < sorted_graph.index(6)
+    assert sorted_graph.index(6) < sorted_graph.index(7)
+
+
+def test_top_sort():
+    top_sort_tester(topsort)
